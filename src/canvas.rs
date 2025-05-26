b3c96cfd4ee8246411c07a02402af27d58d59cd7
Anjishnu Banerjee <inviktor27@gmail.com>
2025-05-26 21:57:48 +0530

    add wireframe triangles

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,7 +1,7 @@
 use crate::utils::{CANVAS_BOUND_X, CANVAS_BOUND_Y, CANVAS_HEIGHT, CANVAS_WIDTH};
 use std::panic;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Point2D(pub i32, pub i32);
 
 pub struct Color(pub u32, pub u32, pub u32);
@@ -37,31 +37,40 @@ impl Canvas {
         self.buffer[screeny * CANVAS_WIDTH + screenx] = color.as_u32();
     }
 
-    pub fn draw_line(&mut self, mut p0: Point2D, mut p1: Point2D, color: &Color) {
-        let (x0, y0) = (p0.0 as f64, p0.1 as f64);
-        let (x1, y1) = (p1.0 as f64, p1.1 as f64);
-
-        if (x0 - x1).abs() > (y0 - y1).abs() {
+    fn draw_line(&mut self, mut p0: Point2D, mut p1: Point2D, color: &Color) {
+        if (p0.0 - p1.0).abs() > (p0.1 - p1.1).abs() {
+            // Mostly horizontal line
             if p0.0 > p1.0 {
                 std::mem::swap(&mut p0, &mut p1);
             }
-
-            let ys = interpolate(p0.0, y0, p1.0, y1);
+            let ys = interpolate(p0.0, p0.1 as f64, p1.0, p1.1 as f64);
             for x in p0.0..=p1.0 {
                 let y = ys[(x - p0.0) as usize];
                 self.put_pixel(Point2D(x, y.round() as i32), color);
             }
         } else {
+            // Mostly vertical line
             if p0.1 > p1.1 {
                 std::mem::swap(&mut p0, &mut p1);
             }
-
-            let xs = interpolate(p0.1, x0, p1.1, x1);
+            let xs = interpolate(p0.1, p0.0 as f64, p1.1, p1.0 as f64);
             for y in p0.1..=p1.1 {
                 let x = xs[(y - p0.1) as usize];
                 self.put_pixel(Point2D(x.round() as i32, y), color);
             }
         }
+    }
+
+    pub fn draw_wireframe_triangle(
+        &mut self,
+        p0: Point2D,
+        p1: Point2D,
+        p2: Point2D,
+        color: &Color,
+    ) {
+        self.draw_line(p0, p1, color);
+        self.draw_line(p1, p2, color);
+        self.draw_line(p2, p0, color);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,12 @@ fn main() {
         panic!("{}", e);
     });
 
-    canvas.draw_line(Point2D(-50, -200), Point2D(60, 120), &WHITE);
+    canvas.draw_wireframe_triangle(
+        Point2D(-200, -250),
+        Point2D(200, 50),
+        Point2D(20, 250),
+        &WHITE,
+    );
 
     window.set_target_fps(60);
     while window.is_open() && !window.is_key_down(Key::Escape) {
